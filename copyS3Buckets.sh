44bca7e4a4d2ef2548aa3078b9c0bac48b0c6dd7
Charles Grimes <CSGrimey@users.noreply.github.com>
2019-04-18 15:19:16 +0100

    Create copyS3Buckets.sh

---
+++ copyS3Buckets.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Usage: bash copyS3Buckets.sh identity arn:aws:iam::1234:role/someRole fromBucketName toBucketName
+
+export AWS_PROFILE='$0'
+export AWS_ROLE_ARN='$1'
+
+aws configure set default.s3.max_concurrent_requests 1000
+aws configure set default.s3.max_queue_size 100000
+
+# Set some environment variables (you need aws cli and jq):
+$(aws sts assume-role --profile "$AWS_PROFILE" --role-arn "$AWS_ROLE_ARN" --role-session-name "my-test-session" | jq -r '"export AWS_ACCESS_KEY_ID=" + .Credentials.AccessKeyId + " AWS_SECRET_ACCESS_KEY=" + .Credentials.SecretAccessKey + " AWS_SESSION_TOKEN=" + .Credentials.SessionToken')
+
+aws s3 sync s3://$2 s3://$3
